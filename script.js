046bb6b3639a05106a820bfc0ca7de2cebe6b6da
aayushi221 <aayushipandey221@gmail.com>
2021-08-24 20:08:52 +0530

    images

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
-$( document ).ready(function() {
-$('html').on('click', '#search-toggle', function() {
-  $('#search').val('');
-  if ($(this).find('span').hasClass('fa-search')) {
-    $('#search').focus();
-  }
-	$('.main-search').toggleClass('active');
-  $(this).find('span').toggleClass('fa-search');
-  $(this).find('span').toggleClass('fa-times');
-});
-});
+// $( document ).ready(function() {
+// $('html').on('click', '#search-toggle', function() {
+//   $('#search').val('');
+//   if ($(this).find('span').hasClass('fa-search')) {
+//     $('#search').focus();
+//   }
+// 	$('.main-search').toggleClass('active');
+//   $(this).find('span').toggleClass('fa-search');
+//   $(this).find('span').toggleClass('fa-times');
+// });
+// });
